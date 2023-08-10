4e9fb2fd9dff64e9eb507f37ccc2701bbdefa79d
Joaquín Frías <joaquin124507@gmail.com>
2023-08-10 16:46:01 -0300

    Resolucion Ejercicio 1 de la primer guia de ejercicios

---
+++ GuiaDeEjercicios1/src/guiadeejercicios1/GuiaDeEjercicios1.java
@@ -0,0 +1,30 @@
+
+package guiadeejercicios1;
+
+import java.util.Scanner;
+
+
+public class GuiaDeEjercicios1 {
+
+    
+    public static void main(String[] args) {
+        
+        //Ejercicio 1
+        /*Hacer un programa para solicitar por teclado un número y luego devolver su
+        valor elevado al cubo.
+        Nota: no olvides que sólo contamos con las cuatro operaciones básicas.*/
+       
+        //Resolución
+        /*
+        int num;
+        System.out.println("Ingrese un número");
+        Scanner teclado = new Scanner(System.in);
+        num = teclado.nextInt();
+        int cubo = (num * num * num);
+        
+        System.out.println("El número elevado al cubo es: " + cubo);
+        */
+        
+    }
+    
+}
