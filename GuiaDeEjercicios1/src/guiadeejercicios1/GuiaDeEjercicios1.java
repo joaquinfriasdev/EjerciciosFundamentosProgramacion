a7754b5dfb54e0babab20fe320d9d0dd144d06d8
Joaquín Frías <joaquin124507@gmail.com>
2023-08-11 14:54:31 -0300

    Ejercicio 3 de la guia de ejercicios 1

--- GuiaDeEjercicios1/src/guiadeejercicios1/GuiaDeEjercicios1.java
+++ GuiaDeEjercicios1/src/guiadeejercicios1/GuiaDeEjercicios1.java
@@ -9,7 +9,7 @@ public class GuiaDeEjercicios1 {
     
     public static void main(String[] args) {
         
-        //Ejercicio 1
+        //Ejercicio 2
         /*Hacer un programa para solicitar por teclado un número y luego devolver su
         valor elevado al cubo.
         Nota: no olvides que sólo contamos con las cuatro operaciones básicas.*/
@@ -23,6 +23,30 @@ public class GuiaDeEjercicios1 {
         int cubo = (num * num * num);
         
         System.out.println("El número elevado al cubo es: " + cubo);
+        */
+        
+        
+        // Ejercicio 3
+        
+        /*
+        Hacer un programa que permita ingresar el año actual y el año de la fecha de nacimiento
+        de una persona y luego calcule y emita por pantalla su edad. 
+        */
+        
+        //Resolución
+        /*
+        int anioActual;
+        int anioNac;
+        System.out.println("Ingrese el año actual");
+        Scanner tecladoAnio = new Scanner(System.in);
+        anioActual = tecladoAnio.nextInt();
+        System.out.println("Ingrese el año de su nacimiento");
+        Scanner tecladoNac = new Scanner(System.in);
+        anioNac = tecladoNac.nextInt();
+        
+        int edad = anioActual - anioNac;
+        
+        System.out.println("Su edad es: " + edad);
         */
         
     }
