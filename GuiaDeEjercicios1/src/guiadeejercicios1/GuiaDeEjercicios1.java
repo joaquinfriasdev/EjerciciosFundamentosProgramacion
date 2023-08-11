fa1fcfd3e48e547efd238a8a56142d0106a49753
Joaquín Frías <joaquin124507@gmail.com>
2023-08-11 16:48:22 -0300

    Rehice el proyecto y cargué todas las consigas

---
+++ GuiaDeEjercicios1/src/guiadeejercicios1/GuiaDeEjercicios1.java
@@ -0,0 +1,82 @@
+
+package guiadeejercicios1;
+
+import java.util.Scanner;
+
+
+public class GuiaDeEjercicios1 {
+
+    public static void main(String[] args) {
+       
+        //Ejercicio 2
+        /*Hacer un programa para solicitar por teclado un número y luego devolver su
+        valor elevado al cubo.*/
+        
+        //Resolución
+        /*
+        int num;
+        System.out.println("Ingrese el número");
+        Scanner teclado = new Scanner(System.in);
+        num = teclado.nextInt();
+        int cubo = (num * num * num);
+        System.out.println("El número al cubo es: " + cubo);
+        */
+        
+        //Ejercicio 3
+        /* Hacer un programa que permita ingresar el año actual y el año de la fecha de
+        nacimiento de una persona y luego calcule y emita por pantalla su edad.*/
+        /*
+        int anioAct;
+        int anioNac;
+        System.out.println("Ingrese el año actual: ");
+        Scanner tecAct = new Scanner(System.in);
+        anioAct = tecAct.nextInt();
+        System.out.println("Ingrese el año de su nacimiento: ");
+        Scanner tecNac = new Scanner(System.in);
+        anioNac = tecNac.nextInt();
+        int edad = (anioAct - anioNac);
+        System.out.println("Su edad es: " + edad);
+        */
+        
+        //Ejercicio 4
+        /*Hacer un programa que permite ingresar los kilómetros existentes entre dos ciudades y la velocidad
+        promedio de un vehículo. Calcular y emitir po pantalla el tiempo aproximado que demandará llegar de un
+        punto a otro teniendo en cuenta los datos ingresados*/
+        
+        //Ejercicio 5
+        /*Una casa de computación paga a sus empleados un sueldo fijo de $15.000 mas una comisión del 5% sobre
+        el total facturado por cada empleado. Hacer un programa para ingresar el total facturado por un empleado
+        y que luego calcule y emita por pantalla el sueldo total a cobrar por el mismo.*/
+        
+        //Ejercicio 6
+        /* Hacer un programa para ingresar por teclado las tres notas de exámenes de un
+        alumno y luego calcule y emita por pantalla el promedio final.*/
+        
+        //Ejercicio 7
+        /* Hacer un programa para ingresar por teclado los metros cuadrados totales de
+        un predio y los metros cuadrados cubiertos; luego calcular y mostrar por
+        pantalla el porcentaje de metros cuadrados cubiertos y el porcentaje de
+        metros cuadrados descubiertos.*/
+        
+        //Ejercicio 8
+        /*Una importante cadena de delivery cuenta con una promoción por tiempo
+        limitado en la que otorga un 15% de descuento sobre el total del valor de la
+        compra realizada. Hacer un programa para solicitar el monto total y el mismo
+        calcule y emita por pantalla el total a cobrar con el descuento aplicado. */
+        
+        //Ejercicio 9
+        /* Una universidad desea conocer los porcentajes de mujeres y hombres en las
+        carreras de ciencias exactas. Se solicita un programa para cargar la cantidad de
+        mujeres y la cantidad de hombres y que el mismo calcule y emita por pantalla
+        los porcentajes correspondientes.*/
+        
+        //Ejercicio 10
+        /*Hacer un programa que permita ingresar por teclado dos números y que luego
+        muestre por pantalla la suma, la resta, la multiplicación y la división de dichos
+        números. Se deben mostrar cuatro resultados en pantalla. Los números deben
+        ser solicitados una única vez. */
+        
+        
+    }
+    
+}
