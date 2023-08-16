a716b18208a1ecbe143568fa69a3c00f45b60cc1
Joaquín Frías <joaquin124507@gmail.com>
2023-08-16 13:44:06 -0300

    Creo la guia de ejercicios 2 y cargo los 12 ejercicios

---
+++ GuiaDeEjercicios2/src/guiadeejercicios2/GuiaDeEjercicios2.java
@@ -0,0 +1,86 @@
+
+package guiadeejercicios2;
+
+
+public class GuiaDeEjercicios2 {
+
+
+    public static void main(String[] args) {
+        
+    //Ejercicio 1
+    /*Hacer un programa para ingresar un número y luego se emita por pantalla un
+    cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.*/
+    
+    int num;
+    Scanner teclado = new Scanner(System.in);
+    
+    
+    //Ejercicio 2
+    /*Hacer un programa para ingresar dos números distintos y luego se muestre por
+    pantalla el menor de ellos.
+    Nota: no te olvides del ambiente ideal.*/
+    
+    
+    //Ejercicio 3
+    /*Hacer un programa para ingresar dos números y que luego emita por pantalla
+    el mayor de ellos o un cartel aclaratorio “Son iguales” en el caso de que así sea.
+    Nota: los números pueden ser iguales.*/
+    
+    
+    //Ejercicio 4
+    /*Hacer un programa para ingresar un número y luego se emita un cartel por
+    pantalla “Positivo” si el número es mayor a cero, “Negativo” si el número es
+    menor a cero o “Cero” si el número es igual a cero.
+    Nota: requiere más de in IF!*/
+    
+    
+    //Ejercicio 5
+    /*Hacer un programa para ingresar un número y mostrar por pantalla un cartel
+    aclaratorio si el mismo es PAR o IMPAR.
+    Nota: leé sobre el operador “Resto”.*/
+    
+    
+    //Ejercicio 6
+    /*Una casa de video juegos otorga un descuento dependiendo del importe de la
+    compra realizada según los siguientes valores:
+    • Si el importe es menor a ARS 1000, no hay descuento.
+    • Si el importe es ARS 1000 o más pero menor a ARS 5000, aplica un
+    descuento del 10%.
+    • Si el importe es ARS 5000 o más, aplica un descuento del 18%.
+    Hacer un programa para ingresar un importe de venta y luego muestre por
+    pantalla el importe final con el descuento que corresponda.*/
+    
+    
+    //Ejercicio 7
+    /*Hacer un programa para ingresar cuatro números distintos y luego mostrar por
+    pantalla el mayor de ellos.*/
+    
+    
+    //Ejercicio 8
+    /*Hacer un programa para ingresar cuatro números distintos y luego mostrar por
+    pantalla el menor de ellos.*/
+    
+    
+    //Ejercicio 9
+    /*Hacer un programa para ingresar cinco números distintos y luego mostrar por
+    pantalla el mayor y el menor de ellos.*/
+    
+    //Ejercicio 10
+    /*Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+    cuáles son mayores a 100.*/
+    
+    
+    //Ejercicio 11
+    /*Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+    cuántos son menores a 100.*/
+    
+    
+    //Ejercicio 12
+    /*Hacer un programa para ingresar un valor que estará expresado en minutos. Si
+    los minutos superan los 60, pasar el valor a horas, de lo contrario dejarlo en
+    minutos. Mostrar el resultado en pantalla aclarando si se muestran minutos u
+    horas.*/
+        
+    }
+    
+}
