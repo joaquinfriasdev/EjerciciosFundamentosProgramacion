311ae836abad74d30ef280883d55b4da420125ea
Joaquín Frías <joaquin124507@gmail.com>
2023-08-24 15:55:36 -0300

    Cree el proyecto de la guia de ejercicios y realice el primer ejercicio

---
+++ GuiaDeEjercicios5/src/guiadeejercicios5/GuiaDeEjercicios5.java
@@ -0,0 +1,34 @@
+
+package guiadeejercicios5;
+
+
+public class GuiaDeEjercicios5 {
+
+
+    public static void main(String[] args) {
+        
+        //Ejercicio 1
+        /*
+        int num = 1;
+        while (num<11) {            
+            System.out.println(num);
+            num++;
+        }
+        */
+        
+        
+        //Ejercicio 2
+        //Ejercicio 3
+        //Ejercicio 4
+        //Ejercicio 5
+        //Ejercicio 6
+        //Ejercicio 7
+        //Ejercicio 8
+        //Ejercicio 9
+        //Ejercicio 10
+        //Ejercicio 11
+        
+        
+    }
+    
+}
