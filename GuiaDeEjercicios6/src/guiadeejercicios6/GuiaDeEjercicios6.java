1a03722b402e150c217efe9f0afdf0fea24a61f9
Joaquín Frías <joaquin124507@gmail.com>
2023-08-29 13:22:34 -0300

    Cree el proyecto para la guia de ejercicios 6

---
+++ GuiaDeEjercicios6/src/guiadeejercicios6/GuiaDeEjercicios6.java
@@ -0,0 +1,21 @@
+
+package guiadeejercicios6;
+
+
+public class GuiaDeEjercicios6 {
+
+    public static void main(String[] args) {
+       
+        //Ejercicio 1
+        
+        //Ejercicio 2
+        
+        //Ejercicio 3
+        
+        //Ejercicio 4
+        
+        //Ejercicio 5
+        
+    }
+    
+}
